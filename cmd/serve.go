56d5d5497acbef121a015e13413fdc5c97804d5f
zhuzongxin <dreamsxin@qq.com>
2024-04-29 09:15:13 +0800

    Add config

--- cmd/serve.go
+++ cmd/serve.go
@@ -4,11 +4,14 @@ Copyright © 2024 NAME HERE <EMAIL ADDRESS>
 package cmd
 
 import (
+	"gin-demo/config"
 	"gin-demo/debug"
 	"gin-demo/middleware"
 	"gin-demo/router"
 	"log"
+	"net/http"
 
+	"github.com/gin-contrib/static"
 	"github.com/gin-gonic/gin"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
@@ -37,5 +40,22 @@ func run() {
 	if isDebug {
 		debug.InitDebug()
 	}
+
+	docroot := config.Server().WebServer.Root
+	if docroot == "" {
+		docroot = "./"
+	}
+	fs := static.LocalFile(docroot, false)
+	fileserver := http.FileServer(fs)
+
+	r.NoRoute(func(c *gin.Context) {
+		prefix := "/"
+		if fs.Exists(prefix, c.Request.URL.Path) {
+			fileserver.ServeHTTP(c.Writer, c.Request)
+			c.Abort()
+		} else {
+			c.AbortWithStatus(http.StatusNotFound)
+		}
+	})
 	r.Run(viper.GetString("addr")) // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
--- config/cfg_server.go
+++ config/cfg_server.go
@@ -1,8 +1,13 @@
 package config
 
 type ServerConfig struct {
-	Name string `mapstructure:"name" json:"name" yaml:"name"` //appname
-	Mode string `mapstructure:"mode" json:"mode" yaml:"mode"` //模式
-	Host string `mapstructure:"host" json:"host" yaml:"host"` //host
-	Port int    `mapstructure:"port" json:"port" yaml:"port"` //port
+	Name      string    `mapstructure:"name" json:"name" yaml:"name"` //appname
+	Mode      string    `mapstructure:"mode" json:"mode" yaml:"mode"` //模式
+	Host      string    `mapstructure:"host" json:"host" yaml:"host"` //host
+	Port      int       `mapstructure:"port" json:"port" yaml:"port"` //port
+	WebServer WebServer `mapstructure:"web-server" json:"web-server" yaml:"web-server"`
+}
+
+type WebServer struct {
+	Root string `mapstructure:"root" json:"root" yaml:"root"`
 }
